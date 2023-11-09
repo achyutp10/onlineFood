71b833d780954572580550f5121292aa87f5f167
achyut <achyutp10@gmail.com>
2023-11-09 19:47:32 +0545

    user registration(Customer)

--- accounts/signals.py
+++ accounts/signals.py
@@ -9,7 +9,7 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
   print(created)
   if created:
     UserProfile.objects.create(user=instance)
-    print('User profile is created')
+    # print('User profile is created')
   else:
     try:
       profile = UserProfile.objects.get(user=instance)
@@ -17,10 +17,11 @@ def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     except:
       #Create the userprofile if not exist 
       UserProfile.objects.create(user=instance)
-      print('Profile was not exits but i created one')
-    print('User is updated')
+      # print('Profile was not exits but i created one')
+    # print('User is updated')
   
 @receiver(pre_save,sender=User)
 def pre_save_profile_receiver(sender, instance, **kwargs):
-  print(instance.username, 'This user is being saved')
+  pass
+  # print(instance.username, 'This user is being saved')
 # post_save.connect(post_save_create_profile_receiver, sender=User)
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,24 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import User
+
+from .forms import UserForm
 
 # Create your views here.
 
 def registerUser(request):
-  return render(request, 'accounts/registerUser.html')
+  if request.method == 'POST': 
+    print(request.POST)
+    form = UserForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.role = User.CUSTOMER
+      form.save()
+      return redirect('registerUser')
+  else:
+    form = UserForm()
+  context = {
+    'form': form,
+  }
+  return render(request, 'accounts/registerUser.html', context)
