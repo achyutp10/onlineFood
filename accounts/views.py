534b65adb766420a5e83049491fcec09d75e61d8
achyut <achyutp10@gmail.com>
2023-11-10 13:05:26 +0545

    Handeling filed_errors and non_field_errors

--- accounts/views.py
+++ accounts/views.py
@@ -9,13 +9,28 @@ from .forms import UserForm
 
 def registerUser(request):
   if request.method == 'POST': 
-    print(request.POST)
     form = UserForm(request.POST)
     if form.is_valid():
-      user = form.save(commit=False)
+      # Create user using the form
+      # password = form.cleaned_data['password']
+      # user = form.save(commit=False)
+      # user.set_password(password)
+      # user.role = User.CUSTOMER
+      # user.save()
+
+      # create the user using the create_user method
+      first_name = form.cleaned_data['first_name']
+      last_name = form.cleaned_data['last_name']
+      username = form.cleaned_data['username']
+      email = form.cleaned_data['email']
+      password = form.cleaned_data['password']
+      user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
       user.role = User.CUSTOMER
-      form.save()
+      user.save()
       return redirect('registerUser')
+    else:
+      print('Invalid form')
+      print(form.errors)
   else:
     form = UserForm()
   context = {
