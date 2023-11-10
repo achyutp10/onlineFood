9a391cb6c1bbd29c8039b038bb43a7dfe70bd2d2
achyut <achyutp10@gmail.com>
2023-11-10 14:50:15 +0545

    Homepage setup and messages animations

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -140,3 +140,10 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
